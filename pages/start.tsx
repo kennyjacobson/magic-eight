b105157bfef0fe32539641da3c244046c0275393
kennyjacobson <kenny.jacobson@gmail.com>
2024-02-11 19:56:24 -0800

    try, try again

--- pages/start.tsx
+++ pages/start.tsx
@@ -1,9 +1,33 @@
 import Image from "next/image";
-import FrameHomePage from "@/app/components/frame-home-page";
+// import FrameHomePage from "@/app/components/frame-home-page";
+import {FrameMetadata} from  '@coinbase/onchainkit'
 export default function Result() {
     return (
 <>
-<FrameHomePage />
+<FrameMetadata
+      buttons={[
+        {
+          label: 'Tell me the story',
+        },
+        {
+          action: 'link',
+          label: 'Link to Google',
+          target: 'https://www.google.com'
+        },
+        {
+          action: 'post_redirect',
+          label: 'Redirect to cute pictures',
+        },
+      ]}
+      image={{
+       src: 'https://zizzamia.xyz/park-3.png',
+       aspectRatio: '1:1'
+      }}
+      input={{
+        text: 'Tell me a boat story',
+      }}
+      postUrl="https://zizzamia.xyz/api/frame"
+    />
            
             <Image
               src="https://nfts-dataw.s3.amazonaws.com/magic-8-ball/magic-8-ball-title.png"
