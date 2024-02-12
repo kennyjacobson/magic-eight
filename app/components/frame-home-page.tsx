4efb2133a0f9c61e04ec7679c307aa50605288e9
kennyjacobson <kenny.jacobson@gmail.com>
2024-02-11 18:43:20 -0800

    more stuff

--- app/components/frame-home-page.tsx
+++ app/components/frame-home-page.tsx
@@ -1,32 +1,40 @@
+'use client'
 import React from 'react';
 import {FrameMetadata} from  '@coinbase/onchainkit'
+import { useSearchParams  } from 'next/navigation'
 
 export default function FrameHomePage() {
+  // const router = useRouter();
+  // const { getMessage } = router.query;
+  const searchParams = useSearchParams()
+  console.log(searchParams)
+
+  let imageSrc = 'https://nfts-dataw.s3.amazonaws.com/magic-8-ball/magic-8-ball-title.png'
+  
+  const getMessage = searchParams.get('getMessage');
+  console.log(getMessage)
+  if (getMessage) {
+    // if we have a querystring, we'll use the querystring to generate a random number
+    // and use that number to generate a random image
+    const randomNumber = Math.floor(Math.random() * 12) + 1;
+    imageSrc = `https://nfts-dataw.s3.amazonaws.com/magic-8-ball/magic-8-ball-${randomNumber}.png`;
+  }
+
     return (
       
       <FrameMetadata
         buttons={[
           {
-            label: 'Tell me the story',
+            label: 'Shake it!',
           },
-          {
-            action: 'link',
-            label: 'Link to Google',
-            target: 'https://www.google.com'
-          },
-          {
-            action: 'post_redirect',
-            label: 'Redirect to cute pictures',
-          },
+         
         ]}
         image={{
-         src: 'https://nfts-dataw.s3.amazonaws.com/magic-8-ball/magic-8-ball-title.png',
+         src: imageSrc,
          aspectRatio: '1:1'
         }}
-        input={{
-          text: 'Tell me a boat story',
-        }}
-        postUrl="https://zizzamia.xyz/api/frame"
+        
+        postUrl="https://bespoke-cajeta-fb55fd.netlify.app/?getMessage=true"
       />
       
     );
